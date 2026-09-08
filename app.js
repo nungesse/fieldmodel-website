030a083327580ad7a3554bcfe4eaa15ad8e69135
nungesse <brettnun@gmail.com>
2026-09-07 20:43:18 -0700

    Add high-level FieldModel explainer after hero

---
+++ app.js
@@ -0,0 +1,7 @@
+'use strict';
+(function(){
+  const s=document.createElement('script');
+  s.src='https://fieldmodel-private-briefing-kofyrgjdz-field-model.vercel.app/app.js';
+  s.defer=true;
+  document.head.appendChild(s);
+})();
